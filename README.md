7225685d236233a99a43307efeb94ee1bfc524f3
aiwithab <abdulahadas@gmail.com>
2020-01-01 22:51:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Quiz Game
+
+Quiz Game application for android as A new Flutter project.
+
+## Getting Started
+
+This is just a fun project created by Abdul Ahad (aiwithab).
+
+A few resources to get you started if this is your first Flutter project:
+
+- [My Reddit: ](https://reddit.com/aiwithab)
+- [My Twitter: ](https://twitter.com/aiwithab)
+
+end...
---
+++ lib/pages/score_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import './landing_page.dart';
+
+class ScorePage extends StatelessWidget {
+  
+  final int score;
+  final int totalQuestion;
+
+  ScorePage(this.score, this.totalQuestion);
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.blueAccent,
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text('Your Score is: ', style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold),),
+          new Text(score.toString() + "/" + totalQuestion.toString(),style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold),),
+          new IconButton(
+            icon: new Icon(Icons.arrow_right),
+            color: Colors.white,
+            iconSize: 50.0,
+            onPressed: () => Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder:  (BuildContext context) => new LandingPage()),(Route route) => route == null ),
+          )
+        ],
+      ),
+    );
+  }
+}
